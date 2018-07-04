98af251d304b82b8b96846556dfc9d9444cfc7bb
Peter <31906734+InwoongLee@users.noreply.github.com>
2018-07-04 10:59:13 +0900

    Add files via upload

---
+++ NTU_Data_Info/main.m
@@ -0,0 +1,11 @@
+root_path = '../NTU_sklt_original/';
+csv_path = '../NTU_sklt_csv_final_3/';
+txt_path = 'Actions_01-49.txt';
+
+make_csv_action_0149(root_path, csv_path, txt_path);
+
+root_path = '../NTU_sklt_original/';
+csv_path = '../NTU_sklt_csv_final_3/';
+txt_path = 'Actions_50-60.txt';
+
+make_csv_action_5060(root_path, csv_path, txt_path);
